d0790e1d1dae6f71730ed498027c332867c43a0e
84kilos <milesphengsykeo@gmail.com>
2026-04-22 20:48:40 -0500

    Tables setup and some frontend

--- teacher/dashboard.php
+++ teacher/dashboard.php
@@ -14,10 +14,35 @@
     <title>Dashboard</title>
     <link rel="stylesheet" href="../assets/css/styles.css">
     <link rel="stylesheet" href="../assets/css/auth.css">
+    <link rel="stylesheet" href="../assets/css/dashboard.css">
 </head>
 <body>
     <h2>Welcome! You are logged in.</h2>
     <a class="bb-auth-link" href="../logout.php">Logout</a>
-
+    <div class="class-previews">
+        <div class="class-preview">
+            <div class="class-preview-name">
+                <p>The Super demon of evil learning class of death</p>
+            </div>
+            <div class="class-preview-count">
+                <p>20 students</p>
+            </div>
+            <div class="class-preview-goto">
+                <p>→</p>
+            </div>
+        </div>
+        <div class="class-preview">
+            <p>hey</p>
+        </div>
+        <div class="class-preview">
+            <p>hey</p>
+        </div>
+        <div class="class-preview">
+            <p>hey</p>
+        </div>
+        <div class="class-preview">
+            <p>hey</p>
+        </div>
+    </div>
 </body>
 </html>
