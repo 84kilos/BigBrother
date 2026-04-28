76a4eb92de701062b2a163295321e9e83ec5720e
84kilos <milesphengsykeo@gmail.com>
2026-04-28 12:12:10 -0500

    new db

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = new mysqli("localhost", "root", "", "BigBrother", "3308"); //remove 3308 for your setup, mine personally starts at port 3308
+    $conn = new mysqli("localhost", "root", "", "attendance_system", "3308"); //remove 3308 for your setup, mine personally starts at port 3308
     if ($conn->connect_error){
         die("Connection failed: " . $conn->connect_error);
     }
